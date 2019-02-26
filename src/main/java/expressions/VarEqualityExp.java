b7e7afcd8fa1c908ca68f99ad084436920e9bcb6
cooljohnny3 <cooljohnny3@gmail.com>
2019-02-25 23:48:49 -0800

    Compiles

--- src/main/java/expressions/VarEqualityExp.java
+++ src/main/java/expressions/VarEqualityExp.java
@@ -1,12 +1,12 @@
 package j2script.expressions;
 
 public class VarEqualityExp implements Exp {
-    public final Exp var;
+    public final VariableExp var;
     public final Exp expression;
 
-    public VarEqualityExp(final Exp var,
+    public VarEqualityExp(final VariableExp var,
                 final Exp expression) {
-        this.var=var;
-        this.expression=expression;
+        this.var = var;
+        this.expression = expression;
     }
 }
