67db193684be708e9b81249c61b0f0693f5b639b
cooljohnny3 <cooljohnny3@gmail.com>
2019-03-15 22:30:16 -0700

    Add BoolExp

---
+++ src/main/java/parser/expressions/BoolExp.java
@@ -0,0 +1,9 @@
+package j2script.expressions;
+
+public class BoolExp implements Exp {
+    public final Boolean bool;
+
+    public BoolExp(final Boolean bool) {
+        this.bool = bool;
+    }
+}
