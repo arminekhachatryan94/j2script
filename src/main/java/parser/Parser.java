e3180e2b2139da633d2ce1e89867f6f1fe5784c8
RobertBedrosian <chrisx32@gmail.com>
2019-05-17 11:37:01 -0700

    asdasd

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -661,7 +661,6 @@ public class Parser {
         }
         else{
             //Extended class is null, do nothing.
-            ensureTokenIs(++resultpos, new GreaterThanToken());
         }
         ensureTokenIs(++resultpos, new LeftCurlyToken());
         CurlyBraceStack.push(1);
